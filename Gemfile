6016baf0ee7003bcb1f264dd11aad020c0ccc9f2
Matt Schaefer <mschaefer@scitent.com>
2012-01-25 16:46:11 -0500

    f-882: added files necessary to make engine into full application

--- Gemfile
+++ Gemfile
@@ -3,7 +3,12 @@ source :rubygems
 gem 'rails', '= 3.1.3'
 
 gem 'bartt-ssl_requirement', '1.3.1', :require => 'ssl_requirement'
+gem 'sqlite3-ruby', :require => 'sqlite3'
 gem 'will_paginate', '~> 3.0'
+
+group :development do
+  gem 'rspec-rails'
+end
 
 group :test do
   gem 'database_cleaner'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,8 +2,14 @@ require 'scidea/schools'
 
 ENV["RAILS_ENV"] ||= 'test'
 
+require File.expand_path("../../config/environment", __FILE__)
+
 require 'rspec/rails'
 require 'rspec_tag_matchers'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :rspec
