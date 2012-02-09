0daf749544c6a41adbe5a4a98d4d28be84ee1011
Matt Schaefer <mschaefer@scitent.com>
2012-02-09 13:12:41 -0500

    f-phx-889: css assets

--- lib/scidea/schools/engine.rb
+++ lib/scidea/schools/engine.rb
@@ -4,17 +4,10 @@ module Scidea
 
       initializer "scidea.schools.register_view_callbacks" do
 
-        Scidea::Plugins::Plugin.on(:registration_new_before_errors)  { 'schools/script_form' }
-        Scidea::Plugins::Plugin.on(:admin_user_form_before_errors)   { 'schools/script_form' }
-        Scidea::Plugins::Plugin.on(:user_profile_form_before_errors) { 'schools/script_form' }
-
-        Scidea::Plugins::Plugin.on(:admin_user_form_after_profile)   { profile_form }
-        Scidea::Plugins::Plugin.on(:registration_new_after_profile)  { profile_form }
-        Scidea::Plugins::Plugin.on(:user_profile_form_after_profile) { profile_form }
-
-        def profile_form
-          School.render_form? ? 'schools/profile_form' : nil
-        end
+        Scidea::Plugins::Plugin.on(:admin_user_form_after_profile)   { conditional_render('admin/users/school_edit') }
+      
+        Scidea::Plugins::Plugin.on(:registration_new_after_profile)  { conditional_render('users/school_edit') }        
+        Scidea::Plugins::Plugin.on(:user_profile_form_after_profile)  { conditional_render('users/school_edit') }
 
         Scidea::Plugins::Plugin.on(:admin_user_profile_after_profile) { 'admin/users/profile_show_school' }
 
@@ -23,6 +16,11 @@ module Scidea
 
       config.to_prepare do
         load 'scidea/schools/models/mixins.rb'
+      end
+
+      private
+      def conditional_render(partial)
+        School.render_form? ? partial : nil
       end
 
     end # class Engine
