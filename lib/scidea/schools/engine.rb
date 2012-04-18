4b6ff6495feda1578fdd5474a4a5cc239e801b77
John Loy <jloy@scitent.com>
2012-04-18 14:36:17 -0400

    f-phx-1196 Modifies some view to work with the new admin theme

--- lib/scidea/schools/engine.rb
+++ lib/scidea/schools/engine.rb
@@ -7,15 +7,15 @@ module Scidea
 
       initializer "scidea.schools.register_view_callbacks" do
         Scidea::Plugins::Plugin.on(:admin_user_form_after_profile)   { conditional_render('users/school_edit') }
-      
-        Scidea::Plugins::Plugin.on(:registration_new_after_profile)  { conditional_render('users/school_edit') }        
+
+        Scidea::Plugins::Plugin.on(:registration_new_after_profile)  { conditional_render('users/school_edit') }
         Scidea::Plugins::Plugin.on(:user_profile_form_after_profile)  { conditional_render('users/school_edit') }
 
         Scidea::Plugins::Plugin.on(:admin_user_profile_after_profile) { 'admin/users/profile_show_school' }
 
         Scidea::Plugins::Plugin.on(:user_profile_page_js) { 'backbone_apps/school_selector' }
 
-        Scidea::Plugins::Plugin.on(:after_admin_primary_navigation_users_menu) do |callback, menu| 
+        Scidea::Plugins::Plugin.on(:admin_primary_navigation_users_menu_append) do |callback, menu|
 
           menu.item :schools, 
                    'Schools',
